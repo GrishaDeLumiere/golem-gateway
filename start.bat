ff8a2b719f649caeada1514d335ac9c7b612d62d
GrishaDeLumiere <58596517+GrishaDeLumiere@users.noreply.github.com>
2026-04-28 18:02:43 +0300

    fix v2

--- start.bat
+++ start.bat
@@ -1,48 +1,7 @@
 @echo off
-setlocal
-chcp 65001 >nul
-cd /d "%~dp0"
-
-title Golem API Router
-color 0B
-
-echo ===============================================
-echo [!] Инициализация системы...
-echo ===============================================
-
-REM Проверка наличия Node.js в системе
-where node >nul 2>nul
-if %errorlevel% neq 0 (
- echo [!] Ошибка: Node.js не установлен или не добавлен в PATH!
- pause
- exit /b
-)
-
-if exist "node_modules\" (
- echo [+] Зависимости найдены.
- goto :run_app
-)
-
-echo [*] Библиотеки не найдены. Начинаю установку...
-echo.
-
-REM CALL обязателен, иначе выполнение прервется
-call npm install --loglevel=info --progress=true
-if %errorlevel% neq 0 (
- echo.
- echo [!] Ошибка установки зависимостей!
- pause
- exit /b
-)
-
-echo.
-echo [+] Установка завершена!
-
-:run_app
-echo.
-echo [*] Запуск маршрутизатора...
-echo.
-
-node start.js
-
+pushd %~dp0
+set NODE_ENV=production
+call npm install --no-save --no-audit --no-fund --loglevel=error --no-progress --omit=dev --ignore-scripts
+node start.js %*
 pause
+popd
